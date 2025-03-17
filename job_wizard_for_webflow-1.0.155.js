a2945fd806114f490b66113d58c04e8c275a384f
HandlingwithChen <work.chencan@gmail.com>
2025-03-17 20:05:35 +0800

    improve phone number logic

--- job_wizard_for_webflow-1.0.155.js
+++ job_wizard_for_webflow-1.0.155.js
@@ -611,28 +611,14 @@ function dofillUserInfo(userInfo) {
   // tab1
   firstNameInput.value = userInfo.basic_info?.first_name;
   lastNameInput.value = userInfo.basic_info?.last_name;
-  let phoneGroup = userInfo.basic_info?.phone_number?.split(" ")
+  const phoneNumber = userInfo.basic_info?.phone || '';
 
-  if (phoneGroup.length > 1) {
-    // 设置默认值
-    // var phonegrid = document.querySelector('.phonegrid');
-    // phonegrid.style.alignItems = 'center';
-    // var iti = window.intlTelInput(phoneNumberInput1, {
-    //   initialCountry: "auto",
-    //   separateDialCode: true,
-    //   customPlaceholder: function (selectedCountryPlaceholder, selectedCountryData) {
-    //     return selectedCountryPlaceholder + " " + selectedCountryData.dialCode;
-    //   }
-    // });
-
-    // 设置默认值
-    if (phoneGroup[0]) {
-      iti.setNumber(phoneGroup[0]);
-      phoneNumberInput1.value = phoneGroup[0];
-    }
-    phoneNumberInput2.value = phoneGroup[1];
+  if (phoneNumber.startsWith('+') && phoneNumber.includes(' ')) {
+    // 找到第一个空格的位置，然后取其后的所有内容
+    const firstSpaceIndex = phoneNumber.indexOf(' ');
+    phoneNumberInput2.value = phoneNumber.substring(firstSpaceIndex + 1);
   } else {
-    phoneNumberInput2.value = userInfo.basic_info?.phone_number;
+    phoneNumberInput2.value = phoneNumber;
   }
 
   emailInput.value = userInfo.basic_info?.email;
